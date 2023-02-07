64f197b858ebc078111b666cf798c7961e16d2b8
Akizon <coike@citruscat.cn>
2023-02-07 12:08:36 +0800

    233

--- MaterialDesignDemo/Feature/Chara.cs
+++ MaterialDesignDemo/Feature/Chara.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MaterialDesignDemo.Feature
 {
+   
     public class Chara
     {
-        public string charPicUrl { get; set; }
+        public string CharUrl { get; set; }
+        public string CityID { get; set; }
+
+        public Chara() { }
+        public Chara(string charUrl, string cityID)
+        {
+            CharUrl = charUrl;
+            CityID = cityID;
+        }
+
+        
     }
 }
---
+++ MaterialDesignDemo/Feature/ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialDesignDemo.Feature
+{
+    partial class ViewModel:ObservableObject
+    {
+        [ObservableProperty]
+        string charUrlA;
+        [RelayCommand]
+        async Task LoadImg(string id = "150")
+        {
+            await GetImg(@"https://webstatic.mihoyo.com/upload/contentweb/2022/09/27/d978cf1ce85e372971bdcf6add639e8f_5385395843160526652.png");
+        }
+        async Task<HttpResponseMessage> GetImg(string url)
+        {
+            HttpRequestMessage httpRequestMessage = new()
+            {
+                RequestUri = new Uri(url),
+                Method = HttpMethod.Get
+            };
+            var httpc = new HttpClient();
+            var response = await httpc.SendAsync(httpRequestMessage);
+            return response;
+        }
+    }
+}
