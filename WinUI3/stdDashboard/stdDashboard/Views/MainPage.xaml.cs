e3e7ac7c3400c82736cdd6dc26753f63348fddc0
Akizon <coike@citruscat.cn>
2023-02-07 22:23:55 +0800

    nahida

--- WinUI3/stdDashboard/stdDashboard/Views/MainPage.xaml.cs
+++ WinUI3/stdDashboard/stdDashboard/Views/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.UI.Xaml.Controls;
 using stdDashboard.Helpers;
 using stdDashboard.ViewModels;
+using Windows.Media.Playback;
 
 namespace stdDashboard.Views;
 
@@ -10,40 +11,14 @@ public sealed partial class MainPage : Page
     {
         get;
     }
-    string SayHello {get
-        {
-            string s = null;
-            DateTime dateTime = new DateTime();
-            switch (dateTime.Hour)
-            {
-                case <= 5:
-                    s = "Hello_Down";
-                    break;
-                case <= 11:
-                    s = "Hello_Morning";
-                    break;
-                case <= 14:
-                    s = "Hello_Noon";
-                    break;
-                case <= 18:
-                    s = "Hello_Afternoon";
-                    break;
-                case <= 22:
-                    s = "Hello_Night";
-                    break;
-                case <= 24:
-                    s = "Hello_Down";
-                    break;
-                default:
-                    break;
-            }
-            return s == null ? "Hello" : s.GetLocalized();
-        }
-        }
-
     public MainPage()
     {
         ViewModel = App.GetService<MainViewModel>();
         InitializeComponent();
+    }
+
+    private void Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+
     }
 }
